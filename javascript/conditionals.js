7c305ca786edde46bd512d934f7363f9cdb94407
SHShazib <shajjadhossain@gmail.com>
2021-07-16 20:13:21 +0600

    firstcommit

---
+++ javascript/conditionals.js
@@ -0,0 +1,25 @@
+// One conditional rule
+
+var num = 10;
+if (num != 10){
+    console.log("This is smallest number than 10.");
+}
+else {
+    console.log("This is  not smallest number than 10.")
+}
+
+
+
+// Two conditional rule
+var jobPaiso = true;
+var saveMoney = 200000;
+
+if (jobPaiso == true && saveMoney >50000) {
+    console.log("Ekhon amader Khawao.")
+}
+else if (jobPaiso== true || saveMoney >100000){
+    console.log("Pore khawaio.");
+}
+else {
+    console.log("Thak khawate hobe na.");
+}
---
+++ javascript/hello.js
@@ -0,0 +1,42 @@
+// floating type Number Plus
+var num1 = 0.1 ;
+var num2 = 0.2;
+
+var result = num1 + num2 ;
+result = result.toFixed(1);
+console.log(result);
+
+console.log(typeof num1);
+console.log(typeof num2);
+
+
+
+// plus,minus,multiply,division,
+var digit1 = 55;
+var digit2= 20;
+var total1 = digit1 + digit2;
+var total2 = digit1 - digit2;
+var total3 = digit1 * digit2;
+var total4 = digit1 / digit2;
+var total5 = digit1 % digit2;
+console.log(total1);
+console.log(total2);
+console.log(total3);
+console.log(total4.toFixed(4));
+console.log(total5);
+
+// work on String
+var shazib = "i am shazib. i am 22 years old. i am a student. ";
+console.log(shazib.toUpperCase());
+
+// absoluteNumber
+var number = -23;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+var number = 27;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
+
+
+// Round Number
+var num = 7.35
